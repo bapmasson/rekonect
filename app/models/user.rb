086173bf4bf7cdfc79cb9ba124fea018516ce6b3
Jonathan Cucculelli <jonathan.cucculelli04@gmail.com>
2025-07-15 20:47:54 +0200

    associations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :contacts
+  has_many :messages
+  has_many :user_badges
+  has_many :badges, through: :user_badges
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
