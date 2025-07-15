87961dc0a54e1c3296552ad382563624a608382d
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-07-15 20:07:58 +0200

    badge model

---
+++ db/migrate/20250715180705_create_badges.rb
@@ -0,0 +1,11 @@
+class CreateBadges < ActiveRecord::Migration[7.1]
+  def change
+    create_table :badges do |t|
+      t.string :title
+      t.text :description
+      t.text :condition_description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_15_180332) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_15_180705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "badges", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "condition_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
