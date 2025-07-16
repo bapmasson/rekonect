22172ccc458af8bbb0a244dd00f1124174d16145
barthelemy terrier <barthelemy.terrier@yahoo.fr>
2025-07-16 21:23:37 +0200

    a little add

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/suggestion_reply'
-  get 'messages/rekonect'
-  get 'messages/create'
-  get 'messages/dismiss'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,6 +6,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "messages/create", to: "messages#create"
+  get "messages/rekonect", to: "messages#rekonect"
+  get "messages/suggestion_reply", to: "messages#suggestion_reply"
+  get "messages/dismiss", to: "messages#dismiss"
 
   # Defines the root path route ("/")
   # root "posts#index"
