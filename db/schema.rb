da1b0fa23b66223ef94ef03ae720793127fd4270
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-07-17 10:59:11 +0200

    nouvelle migration pour les changements discutés avec Jérôme dans la table messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_16_200708) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_17_085329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,7 +69,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_16_200708) do
     t.bigint "contact_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0, null: false
+    t.text "ai_draft"
+    t.text "user_answer"
+    t.integer "status", default: 0
     t.index ["contact_id"], name: "index_messages_on_contact_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
