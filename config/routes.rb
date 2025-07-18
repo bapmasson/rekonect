1047ab3827e6bd4a1c351de365e823b2d2f91108
barthelemy terrier <barthelemy.terrier@yahoo.fr>
2025-07-18 18:29:54 +0200

    changement pour allez avec les controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "dashboard", to: "dashboard#index"
+  get "badges/:id", to: "badges#show", as: :badge
+
+  resources :contacts
+  resources :messages do
+    collection do
+      get :rekonect
+      get :suggestion_reply
+    end
+    member do
+      patch :dismiss
+    end
+  end
+
+  get "profile", to: "user#show"
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
