94b83608ccf4c0a621f785fee98b77dbe6d869c4
audric nomentsoa <audricmarshall@gmail.com>
2025-08-06 18:59:35 +0200

    Render soft

--- app/javascript/controllers/send_message_controller.js
+++ app/javascript/controllers/send_message_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "@hotwired/stimulus"
+/* import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["form"]
@@ -36,3 +36,4 @@ export default class extends Controller {
   this.formTarget.reset()
   } )
 } }
+ */
