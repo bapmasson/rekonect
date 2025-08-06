5e48fbe0356e37fef89ce475f43cb9cea67e7068
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-08-06 18:54:02 +0200

    vue des conversations refaite

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reset-form"
 export default class extends Controller {
+  connect() {
+    this.element.scrollIntoView({ behavior: "smooth" });
+  }
   reset() {
     // Reset le champ de saisie du formulaire
     this.element.reset()
